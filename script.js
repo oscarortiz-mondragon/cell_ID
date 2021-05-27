fe5437a4739413d7c9e5a008e75a193192730d94
oscarortiz-mondragon <66816556+oscarortiz-mondragon@users.noreply.github.com>
2021-05-26 21:28:35 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let cellTypes = document.getElementById("cellTypes")
+let about = document.getElementById("about")
+let quiz = document.getElementById("quiz")
+let up = document.getElementById("up")
+// CHANGED THE COLOR OF NAV BAR ON HOVER
+cellTypes.onmouseover = whiteBackground
+cellTypes.onmouseleave = normalBackground
+
+about.onmouseover = whiteBackground
+about.onmouseleave = normalBackground
+
+quiz.onmouseover = whiteBackground
+quiz.onmouseleave = normalBackground
+
+function whiteBackground (){
+  this.style.cssText = "background-color:#d6f9dd;"
+}
+
+function normalBackground () {
+this.style.cssText = "background-color: black;  transition: ease-out 0.2s all;"
+}
+
+// MOVE BACK TO TOP ON CLICK OF ARROW
+
+up.addEventListener("click",function(){
+  
+
+})
